d346e4d01d78df86e195332d84f7e93bd59d58c9
littlestarling <takto78@gmail.com>
2017-12-03 00:17:06 +0900

    Introduce ActiveHash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
-class User < ApplicationRecord
+class User < ActiveHash::Base
+  self.data = [
+    { id: 1, name: 'Alice', account_ids: [1, 3, 5] },
+    { id: 2, name: 'Bob', account_ids: [2, 4] },
+    { id: 3, name: 'Carol', account_ids: [6] },
+    { id: 4, name: 'Dave', account_ids: [7, 8] },
+  ]
 end
