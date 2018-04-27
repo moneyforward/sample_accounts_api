6499a4c0e765d89bfd3c8528e53e6dab7e953e12
hkdnet <satoko.itse@gmail.com>
2018-04-27 15:59:02 +0900

    Api 配下に移動

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  class UsersController < ::Api::ApplicationController
+    before_action :set_user, only: [:show]
+
+    # GET /users/1
+    def show
+      render json: @user
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = User.find(params[:id])
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, shallow: true, only: [:show] do
-    resources :accounts, only: [:index, :show]
+  scope module: :api do
+    resources :users, shallow: true, only: [:show] do
+      resources :accounts, only: [:index, :show]
+    end
   end
 end
