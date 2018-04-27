5d47f11bdeb159c2638785198039a916e72e3f95
hkdnet <satoko.itse@gmail.com>
2018-04-27 15:35:31 +0900

    テストを修正

---
+++ test/controllers/accounts_controller_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class AccountsControllerTest < ActionDispatch::IntegrationTest
+  test "should show the user's accounts" do
+    get '/users/1/accounts'
+    assert_response :success
+
+    resp = JSON.parse(response.body)
+    expected = [
+      {
+        "attributes" => {
+          "id" => 1,
+          "user_id" => 1,
+          "name" => "A銀行",
+          "balance" => 20000
+        }
+      },
+      {
+        "attributes" => {
+          "id" => 3,
+          "user_id" => 1,
+          "name" => "C信用金庫",
+          "balance" => 120000
+        }
+      },
+      {
+        "attributes" => {
+          "id" => 5,
+          "user_id" => 1,
+          "name" => "E銀行",
+          "balance" => 5000
+        }
+      }
+    ]
+    assert_equal(expected, resp)
+  end
+
+  test "should show an account" do
+    get "/accounts/2"
+    assert_response :success
+
+    resp = JSON.parse(response.body)
+    expected = {
+      "attributes" => {
+        "id" => 2,
+        "user_id" => 2,
+        "name" => "Bカード",
+        "balance" => 200
+      }
+    }
+    assert_equal(expected, resp)
+  end
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,38 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
-
-  test "should get index" do
-    get users_url, as: :json
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: {  } }, as: :json
-    end
-
-    assert_response 201
-  end
-
   test "should show user" do
-    get user_url(@user), as: :json
+    get '/users/1'
     assert_response :success
-  end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: {  } }, as: :json
-    assert_response 200
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user), as: :json
-    end
-
-    assert_response 204
+    resp = JSON.parse(response.body)
+    expected = {
+      "attributes" => {
+      "id" => 1,
+      "name" => "Alice",
+      "account_ids" => [1, 3, 5],
+      }
+    }
+    assert_equal(expected, resp)
   end
 end
