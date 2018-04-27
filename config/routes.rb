05063d0913304d0c34ac3edad275b8c390e39ee1
hkdnet <satoko.itse@gmail.com>
2018-04-27 16:03:08 +0900

    home#index で html を返すようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
   scope module: :api do
     resources :users, shallow: true, only: [:show] do
       resources :accounts, only: [:index, :show]
