15c74786220c7f3c54830de8bc696f11a5c95ea4
hkdnet <satoko.itse@gmail.com>
2018-05-01 11:56:24 +0900

    Add test

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -35,6 +35,14 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_equal(expected, resp)
   end
 
+  test "cors /users/1/accounts" do
+    get '/users/1/accounts', headers: { Origin: "foo.example.com" }
+
+    assert_response :success
+
+    assert_equal "*", response.headers["Access-Control-Allow-Origin"]
+  end
+
   test "should show an account" do
     get "/accounts/2"
     assert_response :success
