d8e5e7607fae45d0ba88679506edc2f70bf62712
hkdnet <satoko.itse@gmail.com>
2018-05-01 11:58:44 +0900

    allow する path を絞る

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    %w[/users/* /accounts/*].each do |path|
+      resource path,
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
 end
