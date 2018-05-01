5b7cdef648cd664524b1c29ce3cc7f21afeb1051
hkdnet <satoko.itse@gmail.com>
2018-05-01 13:05:36 +0900

    ついでなので render index のテストを追加

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "render index" do
+    get '/'
+
+    assert_response :success
+
+    assert_template :index
+    assert_template layout: "layouts/application"
+  end
+
   test "cors" do
     get '/', headers: { Origin: "foo.example.com" }
 
